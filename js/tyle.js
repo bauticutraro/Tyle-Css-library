7c45c3e2b175ee66b8869f013f8c9a296470b83c
bauticutraro <bautistacutraro@gmail.com>
2018-02-05 01:07:52 -0300

    I added the modal component

---
+++ js/tyle.js
@@ -0,0 +1,26 @@
+// modal
+let modals = document.querySelectorAll('.modal');
+
+document.addEventListener('click', e => {
+    // data-open
+    if (e.target.getAttribute('data-open')) {
+        let targetValue = e.target.getAttribute('data-open');
+
+        let element = Array.from(modals).find(el => el.id === targetValue);
+        if (element) {
+            element.classList.add('show');
+        }
+    }
+
+    // data-close
+    if (e.target.getAttribute('data-close')) {
+        let targetValue = e.target.getAttribute('data-close');
+        
+        let element = Array.from(modals).find(el => el.id === targetValue);
+        if (element) {
+            element.classList.add('animation');
+            setTimeout(() => element.classList.remove('show'), 200);
+            setTimeout(() => element.classList.remove('animation'), 200);            
+        }
+    }
+});
